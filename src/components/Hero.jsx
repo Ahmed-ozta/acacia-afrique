8134f95014f9c8e04028747082719f9e734366d9
Ahmed Chanane <ahmedchanane90@gmail.com>
2025-06-22 21:24:38 +0100

    fix the links path 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ function Hero() {
     return (
         <section className="hero-container">
             <video autoPlay muted loop className="hero-video">
-                <source src="hero-video.mp4" type="video/mp4" />
+                <source src="/acacia-afrique/hero-video.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
             <div className="hero-content">
